81a9f07a499fcef671dacfa8e5d15b1bd74d8e03
dirkmcpherson <dirkmcpherson@gmail.com>
2021-12-06 20:54:28 -0500

    nodes and messages

---
+++ src/control_to_servo.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+from std_msgs.msg import String
+from me134_final.msg import Controller, ArmState
+
+
+PREFIX = "control_to_servo"
+MIN_CONTROL = -1.0
+MAX_CONTROL = 1.0
+
+MIN_SERVO = 0
+MAX_SERVO = 180
+ZERO_SERVO = int((MAX_SERVO - MIN_SERVO) / 2.)
+
+N_JOINTS = 3 #3 joints
+N_JOINTS *= 2 #2 arms
+
+# Set specific joint servo ranges here. They’ll be respected in cap_and_map
+JOINT_SERVO_LIMITS = {vals:(MIN_SERVO, MAX_SERVO) for vals in range(N_JOINTS)}
+# e.g. set the range of the first joint
+# JOINT_LIMITS[0] = (-10, 10)
+
+def map_between_ranges(value,min0,max0,min1,max1):
+    ## quick lifted from: https://stackoverflow.com/questions/1969240/mapping-a-range-of-values-to-another
+    # Figure out how ‘wide’ each range is
+    leftSpan = max0 - min0
+    rightSpan = max1 - min1
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - min0) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    ret = min1 + (valueScaled * rightSpan)
+    
+    print(f"Converted {value} to {ret}")
+    return ret
+
+'''
+Take in radians for each joint. Clean and cap and output a list of servo positions
+'''
+class RadiansToServo:
+    def __init__(self):
+        self.radian_sub = rospy.Subscriber("controller", Controller, self.msg_in, queue_size=10)
+        self.servo_pub = rospy.Publisher(PREFIX+"/out", ArmState, queue_size=10)
+
+        initial_msg = ArmState()
+        initial_msg.left_shoulder = ZERO_SERVO
+        initial_msg.left_elbow = ZERO_SERVO
+        initial_msg.left_finger = ZERO_SERVO
+        initial_msg.right_shoulder = ZERO_SERVO
+        initial_msg.right_elbow = ZERO_SERVO
+        initial_msg.right_finger = ZERO_SERVO
+
+        self.servo_pub.publish(initial_msg)
+
+    def cap_and_map(self, controller_axes_values):
+        if (len(radian_values) < 4):
+            print(f"WARNING: Too few joint values! Expected {4} got {len(controller_axes_values)}")
+
+        capped = [max(min(MAX_CONTROL, entry), MIN_CONTROL) for entry in controller_axes_values]
+
+        mapped = []
+        for i in range(len(capped)):
+            jmin_limit, jmax_limit = JOINT_SERVO_LIMITS[i]
+            if (jmin_limit < MIN_SERVO): print(f"WARN: Servo {i} min value of {jmin_limit} is below global min {MIN_SERVO}")
+            if (jmax_limit < MIN_SERVO): print(f"WARN: Servo {i} max value of {jmax_limit} is below global min {MAX_SERVO}")
+            mapped.append(map_between_ranges(capped[i], MIN_CONTROL, MAX_CONTROL, jmin_limit, jmax_limit))
+        return mapped
+
+    def msg_in(self, controller_msg: Controller):
+        # if any(b for b in controller_msg.button_state[4:8]):
+        print(f"Heard {controller_msg}")
+        ret = ArmState()
+
+        pp = self.cap_and_map(controller_msg.axis_state)
+
+        ret.left_shoulder = pp[0]
+        ret.left_elbow = pp[1]
+        ret.right_shoulder = pp[2]
+        ret.right_elbow = pp[3]
+
+        ret.left_finger = controller_msg.buttons_state[6]
+        ret.right_finger = controller_msg.buttons_state[7]
+
+        self.servo_pub.publish(ret)
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node(PREFIX, anonymous=False)
+        node = RadiansToServo()
+        rospy.spin()
+        # rate = rospy.Rate(10) # 10hz
+        # while not rospy.is_shutdown():
+        #     rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
---
+++ src/servo_hat_commander.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+import rospy
+from ddynamic_reconfigure_python.ddynamic_reconfigure import DDynamicReconfigure
+from me134.msg import WheelState, TorsoState
+
+# constants representing the servo state
+# fmt:off
+LEFT_WHEEL  = 0
+RIGHT_WHEEL = 4
+CHEST       = 8
+LEFT_ELBOW  = 9
+RIGHT_ELBOW = 10
+HEAD        = 11
+# fmt:on
+
+
+class ServoController(object):
+    def __init__(self):
+        rospy.init_node("ServoController", anonymous=False)
+        self.ddynrec = DDynamicReconfigure("")
+
+        self.servo_min_pwm: int = 0
+        self.servo_max_pwm: int = 0
+        self.ddynrec.add_variable("servo_min_pwm", "servo_min_pwm", 0, 0, 2 ** 12)
+        self.ddynrec.add_variable("servo_max_pwm", "servo_max_pwm", 0, 0, 2 ** 12)
+
+        self.add_variables_to_self()
+        self.ddynrec.start(self.dyn_rec_callback)
+
+        self.simulation_mode = True
+        self.i2c_bus = None
+        self.pca = None
+        try:
+            from board import SCL, SDA  # type:ignore
+            import busio  # type:ignore
+            from adafruit_pca9685 import PCA9685  # type:ignore
+
+            self.i2c_bus = busio.I2C(SCL, SDA)
+            self.pca = PCA9685(self.i2c_bus)
+            self.pca.frequency = 60
+
+            self.simulation_mode = False
+        except ImportError:
+            print("WARN: Could not set up hardware dependencies. Assuming we're in sim mode and going from here.")
+
+        rospy.Subscriber("torso_control", TorsoState, self.torso_control_cb)
+
+
+    def add_variables_to_self(self):
+        var_names = self.ddynrec.get_variable_names()
+        for var_name in var_names:
+            self.__setattr__(var_name, None)
+
+    def dyn_rec_callback(self, config, level):
+        del level
+        var_names = self.ddynrec.get_variable_names()
+        for var_name in var_names:
+            self.__dict__[var_name] = config[var_name]
+        return config
+
+    def torso_control_cb(self, msg: TorsoState):
+        # fmt:off
+        # retrieve values and ensure they're between -1 and 1
+        chest       = max(-1, min(1, msg.chest))
+        left_elbow  = max(-1, min(1, msg.left_elbow))
+        right_elbow = max(-1, min(1, msg.right_elbow))
+        head        = max(-1, min(1, msg.head))
+
+        # convert each of these -1 to 1 values into the range of pwm values
+        chest = ((chest + 1) / 2) * (self.servo_max_pwm - self.servo_min_pwm)
+        left_elbow = ((left_elbow + 1) / 2) * (self.servo_max_pwm - self.servo_min_pwm)
+        right_elbow = ((right_elbow + 1) / 2) * (self.servo_max_pwm - self.servo_min_pwm)
+        head = ((head + 1) / 2) * (self.servo_max_pwm - self.servo_min_pwm)
+
+        chest = max(self.servo_min_pwm, min(self.servo_max_pwm, chest))
+        left_elbow = max(self.servo_min_pwm, min(self.servo_max_pwm, left_elbow))
+        right_elbow = max(self.servo_min_pwm, min(self.servo_max_pwm, right_elbow))
+        head = max(self.servo_min_pwm, min(self.servo_max_pwm, head))
+
+        # send pwm values
+        if self.pca is not None:
+            self.pca.channels[CHEST].duty_cycle       = int(chest)
+            self.pca.channels[LEFT_ELBOW].duty_cycle  = int(left_elbow)
+            self.pca.channels[RIGHT_ELBOW].duty_cycle = int(right_elbow)
+            self.pca.channels[HEAD].duty_cycle        = int(head)
+        # fmt:on
+
+def main():
+    sc = ServoController()
+    rospy.loginfo("spinning servo controller")
+    rospy.spin()
+    rospy.loginfo("servo controller shutting down")
+
+
+if __name__ == "__main__":
+    main()
