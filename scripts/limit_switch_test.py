c1b3037a9c1e7debc43e426ce98adc5990cd20df
dirkmcpherson <dirkmcpherson@gmail.com>
2021-12-16 10:22:16 -0500

    limit switch test

---
+++ scripts/limit_switch_test.py
@@ -0,0 +1,76 @@
+from adafruit_servokit import ServoKit
+import sys
+import time
+import rospy
+import time
+
+import RPi.GPIO as gpio
+gpio.setmode(gpio.BCM)
+
+gpio.setup(17, gpio.OUT)
+gpio.setup(22, gpio.OUT)
+gpio.setup(21, gpio.IN, pull_up_down=GPIO.PUD_UP)
+
+# Servo motor for changing the angle up or down
+kit = ServoKit(channels=16)
+
+LEFT_FINGER = 2
+RIGHT_FINGER = 5
+
+WIND_TIME_I = 0.65
+WIND_TIME_II = 0.55
+WIND_THROTTLE = -1.0
+UNWIND_TIME = 1.05
+UNWIND_THROTTLE = -WIND_THROTTLE
+
+if __name__ == "__main__":
+    # device = int(sys.argv[1])
+    # cmd = int(sys.argv[2])
+
+    # open = cmd > 0
+    # close = not open
+
+    # import the libraries
+    import RPi.GPIO as GPIO
+    from time import sleep
+    GPIO.setmode(GPIO.BCM)                   
+    # set the pin numbers to be used from Broadcom chip
+    limit_switch = 4 # assign a variable name to pin 17
+    GPIO.setup(limit_switch, GPIO.IN) # set GPIO pin 17 as Input
+    GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW) # set the initial output of pin 4 to be LOW
+
+    while True:
+        print(GPIO.input(limit_switch))
+        sleep(0.2)
+        #    GPIO.output(ledpin, not GPIO.input(pushpin)) # read the inverse value of input pin 17
+
+    # if (device == 0 and close):
+    #     print("WINDING LEFT")
+    #     kit.continuous_servo[LEFT_FINGER].throttle = WIND_THROTTLE
+    #     time.sleep(WIND_TIME_I)
+    #     kit.continuous_servo[LEFT_FINGER].throttle = -0.5
+    #     time.sleep(WIND_TIME_II)
+    #     kit.continuous_servo[LEFT_FINGER].throttle = 0.0
+    # elif (device == 0 and open):
+    #     print("UNWINDING LEFT")
+    #     kit.continuous_servo[LEFT_FINGER].throttle = UNWIND_THROTTLE
+    #     time.sleep(UNWIND_TIME)
+    #     kit.continuous_servo[LEFT_FINGER].throttle = 0.0
+
+    # if (device == 1 and close):
+    #     print("WINDING RIGHT")
+    #     kit.continuous_servo[RIGHT_FINGER].throttle = WIND_THROTTLE
+    #     time.sleep(WIND_TIME_I)
+    #     kit.continuous_servo[RIGHT_FINGER].throttle = -0.5
+    #     time.sleep(WIND_TIME_II)
+    #     kit.continuous_servo[RIGHT_FINGER].throttle = 0.0
+    # elif (device == 1 and open):
+    #     print("UNWINDING RIGHT")
+    #     kit.continuous_servo[RIGHT_FINGER].throttle = UNWIND_THROTTLE
+    #     time.sleep(UNWIND_TIME)
+    #     kit.continuous_servo[RIGHT_FINGER].throttle = 0.0
+
+    # kit.continuous_servo[RIGHT_FINGER].throttle = 0.0
+    # kit.continuous_servo[LEFT_FINGER].throttle = 0.0
+
+
