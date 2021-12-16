4fd914fc7b2fd1365e8b9bc71aa209ffbce0f712
dirkmcpherson <dirkmcpherson@gmail.com>
2021-12-16 09:50:20 -0500

    longer open V

--- scripts/open_close_test.py
+++ scripts/open_close_test.py
@@ -10,7 +10,8 @@ kit = ServoKit(channels=16)
 LEFT_FINGER = 2
 RIGHT_FINGER = 5
 
-WIND_TIME = 0.25
+WIND_TIME_I = 0.65
+WIND_TIME_II = 0.55
 WIND_THROTTLE = -1.0
 UNWIND_TIME = 1.025
 UNWIND_THROTTLE = -WIND_THROTTLE
@@ -25,9 +26,9 @@ if __name__ == "__main__":
     if (device == 0 and close):
         print("WINDING LEFT")
         kit.continuous_servo[LEFT_FINGER].throttle = WIND_THROTTLE
-        time.sleep(0.65)
+        time.sleep(WIND_TIME_I)
         kit.continuous_servo[LEFT_FINGER].throttle = -0.5
-        time.sleep(0.5)
+        time.sleep(WIND_TIME_II)
         kit.continuous_servo[LEFT_FINGER].throttle = 0.0
     elif (device == 0 and open):
         print("UNWINDING LEFT")
@@ -38,9 +39,9 @@ if __name__ == "__main__":
     if (device == 1 and close):
         print("WINDING RIGHT")
         kit.continuous_servo[RIGHT_FINGER].throttle = WIND_THROTTLE
-        time.sleep(0.65)
+        time.sleep(WIND_TIME_I)
         kit.continuous_servo[RIGHT_FINGER].throttle = -0.5
-        time.sleep(0.5)
+        time.sleep(WIND_TIME_II)
         kit.continuous_servo[RIGHT_FINGER].throttle = 0.0
     elif (device == 1 and open):
         print("UNWINDING RIGHT")
